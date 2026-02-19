036ee8b0d7c0c68cc51a9de460614a5fbbff53a0
Alexander <pchp250g8@tutamail.com>
2026-02-19 12:41:49 +0300

    Добавьте файлы проекта.

---
+++ DecToBin/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input an unsigned integer number");
+var bIsRightNumber = (uint.TryParse(Console.ReadLine(), out var uNumber)) &&
+                     (uNumber <= uint.MaxValue);
+if(!bIsRightNumber)
+{
+    Console.WriteLine("Invalid number format or number too big");
+    Console.Read();
+    return;
+}
+var strBinNum = "";
+var uTempVal = uNumber;
+while(uTempVal > 0)
+{
+    strBinNum = (uTempVal % 2) + strBinNum;
+    uTempVal /= 2;
+}
+if (strBinNum.Length == 0)
+    strBinNum = "0";
+Console.WriteLine
+(
+    "The binary equavalent of the decimal number {0} is: {1}",
+    uNumber,strBinNum
+);
+Console.Read();
